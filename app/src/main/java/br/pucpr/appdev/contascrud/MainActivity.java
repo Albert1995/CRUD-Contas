53f7274387a4e469fb429bd1cf0915a9a5bb28a0
Albert Weihermann <albert.weihermann@hotmail.com>
2018-08-06 22:03:57 -0300

    Created the activities Main and Sign Up.

--- app/src/main/java/br/pucpr/appdev/contascrud/MainActivity.java
+++ app/src/main/java/br/pucpr/appdev/contascrud/MainActivity.java
@@ -1,13 +1,21 @@
 package br.pucpr.appdev.contascrud;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void beginOnClick(View v) {
+        Intent i = new Intent(this, SignUnActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/br/pucpr/appdev/contascrud/SignUnActivity.java
@@ -0,0 +1,13 @@
+package br.pucpr.appdev.contascrud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_un);
+    }
+}
